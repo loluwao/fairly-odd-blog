df96432663132e7095d450593ce6ce55ae7ac580
Temi A <63917134+loluwao@users.noreply.github.com>
2025-12-18 09:36:31 -0500

    Update PageWrapper.tsx

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export const PageWrapper: React.FC<{ children: ReactNode }> = ({
   children,
